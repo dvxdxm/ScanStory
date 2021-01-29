ee7a3e4e485a315c4bf5a40dfb46a9450be96d61
kentViet <dvxdxm@gmail.com>
2021-01-29 18:21:05 +0700

    web

---
+++ truyendochay/truyendochay/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace truyendochay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
