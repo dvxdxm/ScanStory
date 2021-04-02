cc90269a7ba72d9bacce7206bf4e8a02e3d20065
VietDV <dvxdxm@gmail.com>
2021-04-02 17:39:02 +0700

    update morelinq

--- eStoryContainer/eStoryContainer.Services/Chapters/ChapterService.cs
+++ eStoryContainer/eStoryContainer.Services/Chapters/ChapterService.cs
@@ -6,6 +6,7 @@ using eStoryContainer.Core.ViewModels;
 using eStoryContainer.Data;
 using eStoryContainer.Services.Stories;
 using Microsoft.Extensions.Logging;
+using MoreLinq.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -30,7 +31,8 @@ namespace eStoryContainer.Services.Chapters
 
         public List<ChapterViewModel> NewChaptersUpdate(int pageIndex, int page)
         {
-            var chapters = _dbContext.Chapters.Where(chapter => true).Skip((pageIndex + 1) * page).Take(page).OrderByDescending(s => s.modified_on).ToList();
+            var chapters = _dbContext.Chapters.Where(chapter => true).DistinctBy(d => d.story_name).Skip((pageIndex + 1) * page).Take(page).OrderByDescending(s => s.modified_on);
+
             foreach (var item in chapters)
             {
                 item.Story = _storyService.GetBySlug(item.slug);
